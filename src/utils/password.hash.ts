0d7c6e8f07a2de965f47847edea2c62a284bccbf
om-baji <ombaji124@gmail.com>
2025-03-18 13:45:45 +0530

    fix:changed password hashing to argon2

--- src/utils/password.hash.ts
+++ src/utils/password.hash.ts
@@ -1,23 +1,14 @@
-import scrypt from "scrypt-js";
-import crypto from "crypto";
-
+import argon2 from "argon2";
 
 export const hash = async (password: string) => {
-  const salt = crypto.getRandomValues(new Uint8Array(16));
-  const passwordBuffer = new TextEncoder().encode(password);
-
-  const derivedKey = await scrypt.scrypt(passwordBuffer, salt, 16384, 8, 1, 32);
-
-  return `${Buffer.from(salt).toString("hex")}:${Buffer.from(derivedKey).toString("hex")}`;
+  return await argon2.hash(password, {
+    type: argon2.argon2id,
+    memoryCost: 65536,
+    timeCost: 3,
+    parallelism: 2,
+  });
 };
 
 export const verifyPassword = async (password: string, storedHash: string) => {
-  const [saltHex, hashHex] = storedHash.split(":");
-  const salt = new Uint8Array(Buffer.from(saltHex, "hex"));
-  const storedKey = new Uint8Array(Buffer.from(hashHex, "hex"));
-
-  const passwordBuffer = new TextEncoder().encode(password);
-  const derivedKey = await scrypt.scrypt(passwordBuffer, salt, 16384, 8, 1, 32);
-
-  return derivedKey.every((byte, i) => byte === storedKey[i]);
+  return await argon2.verify(storedHash, password);
 };
